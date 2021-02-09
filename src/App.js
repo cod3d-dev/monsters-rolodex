5244d76079849d9024cac547e665f5d36f148d96
Cesar Octavio Delgado <codelgado@gmail.com>
2021-02-09 18:58:45 -0400

    Initial files

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Component } from  'react';
+import './App.css';
+
+import { CardList } from './components/card-list/card-list.component.jsx';
+import {SearchBox} from './components/search-box/seach-box.component.jsx';
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+
+    
+  }
+
+  handleChange = (e) => {
+    this.setState( { searchField: e.target.value });
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));   
+  }
+  render(){
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+    return (
+      <div className="App">
+        <SearchBox
+        placeholder='search monsters'
+        handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />       
+      </div>
+      
+    );
+  }
+}
+
+export default App;
